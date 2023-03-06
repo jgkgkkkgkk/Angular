e1ee98a8ec9b5dfe9aead073c8b4a4e87e07c860
Pasha <Pascha1998ga@mail.ru>
2023-03-06 17:41:44 +0300

    angular init

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-first-project';
+
+  constructor(private router: Router) {
+
+  }
+
+  onRegister(): void {
+      console.log('register click');
+      this.router.navigate(['register']);
+  }
+
+  onAbout(): void {
+    console.log('about click');
+    this.router.navigate(['about'])
+  }
+}
